1385262d9cc3d343f6d85d078469f81aa210f4c7
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2022-12-28 21:02:41 -0300

    criação do projeto

---
+++ src/main/java/br/com/sigatransportes/SigaTransportesApplication.java
@@ -0,0 +1,13 @@
+package br.com.sigatransportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaTransportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaTransportesApplication.class, args);
+	}
+
+}
